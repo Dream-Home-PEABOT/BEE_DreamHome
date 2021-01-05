bd2d321b1ffdb0b2b4a57fe22c5c5834e3a23990
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-05 12:43:13 -0600

    Moves education.py document into models directory and adds brainstorm for api endpoints of Education Model

--- database/education.py
+++
@@ -1,6 +0,0 @@
-from .db import db
-
-class Education(db.Document):
-    question = db.StringField(required=True)
-    explanation = db.StringField(required=True)
-    source_link = db.StringField(required=True)
