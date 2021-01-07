7e412fd2112374b11249539d4e4cb66de221dc23
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-06 20:44:52 -0600

    Adds api/v1 to our routes and controller.
    Co-Authored-By: Priya Power <49959312+priyapower@users.noreply.github.com>
    Co-Authored-By: Eric Hale <eric_hale@comcast.net>

--- api/controller/EducationController.py
+++ api/controller/EducationController.py
@@ -3,7 +3,6 @@ from api.models.education import Education
 from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist, ValidationError, InvalidQueryError
 from api.helpers.errors import EducationAlreadyExistsError
 from flask import render_template
-import pry
 
 class EducationController():
     def __init__(self):
@@ -14,15 +13,14 @@ class EducationController():
         return render_template('education_documentation.html')
 
     def create(self):
-        try:
-            body = request.get_json()
-            education = Education(**body) #will require auth later
-            education.save()
-            id = education.id
-            return {"id": str(id)}, 200
-        except NotUniqueError():
-            pry()
-            raise EducationAlreadyExistsError
-        except Exception:
-            raise InternalServerError
+        # try:
+        body = request.get_json()
+        education = Education(**body) #will require auth later
+        education.save()
+        id = education.id
+        return {"id": str(id)}, 200
+        # except NotUniqueError():
+        #     raise EducationAlreadyExistsError
+        # except Exception:
+        #     raise InternalServerError
 educationcontroller = EducationController()
