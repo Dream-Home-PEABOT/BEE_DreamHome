4deea8b3617bc44874f70befa01582053714bb6a
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-06 20:45:39 -0600

    Adds initial testing
    Co-Authored-By: Priya Power <49959312+priyapower@users.noreply.github.com>
    Co-Authored-By: Eric Hale <eric_hale@comcast.net>

---
+++ tests/BaseCase.py
@@ -0,0 +1,16 @@
+import unittest
+from dreamhome import app
+from database.db import db
+
+class BaseCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client() #talks to flask
+        self.db = db.get_db() #talks to database
+            # add records here (maybe) given, when, then
+
+    def tearDown(self):
+        for collection in self.db.list_collection_names():
+            self.db.drop_collection(collection)
+
+if __name__ == '__main__':
+    unittest.main()
