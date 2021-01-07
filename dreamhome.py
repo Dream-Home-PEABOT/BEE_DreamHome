85713bd75c6f92798f9d134fc01c85c40fe556c0
Priya Power <49959312+priyapower@users.noreply.github.com>
2021-01-06 21:32:26 -0700

    Add an initial welcome test
    - Removes comments on BaseCase
    - Adds whitespace on runner
    - Creates a passing test for `/` in runner (needs it's own SRPed file)
    
    Co-Authored-By: Arique <Arique1104@users.noreply.github.com>

--- dreamhome.py
+++ dreamhome.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from database.db import initialize_db
 from api.routes.api_routes import api
+
 app = Flask(__name__, template_folder = 'api/views')
 
 app.config.from_envvar('ENV_FILE_LOCATION')
 
 initialize_db(app)
 
-@app.route("/")
+@app.route('/')
 def hello():
     return 'Welcome to your dream home server!'
+
 app.register_blueprint(api)
 
 app.run()
--- tests/BaseCase.py
+++ tests/BaseCase.py
@@ -4,9 +4,8 @@ from database.db import db
 
 class BaseCase(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client() #talks to flask
-        self.db = db.get_db() #talks to database
-            # add records here (maybe) given, when, then
+        self.app = app.test_client()
+        self.db = db.get_db()
 
     def tearDown(self):
         for collection in self.db.list_collection_names():
