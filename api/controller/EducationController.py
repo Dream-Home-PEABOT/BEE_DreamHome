627163feef5c7b65e80d9a38265c6fa21db1409a
Priya Power <49959312+priyapower@users.noreply.github.com>
2021-01-07 13:40:33 -0700

    Expose all 5 CRUD endpoint exposures through education endpoint
    - For update and destroy, because python doesn't have an applicable wait 
    response for waiting for database, it is currently returning id and 
    information for checking via GET
    - Would be nice to figure out how to ensure it waits for the database to 
    update

--- api/controller/EducationController.py
+++ api/controller/EducationController.py
@@ -3,16 +3,45 @@ from api.models.education import Education
 from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist, ValidationError, InvalidQueryError
 from api.helpers.errors import EducationAlreadyExistsError
 from flask import render_template
+from re import sub
+import pry
 
 class EducationController():
-    def __init__(self):
-        pass
-    # def get(self):
+    # GET single
+    def get_education(self, id):
+        education = Education.objects.get(id=id).to_json()
+        return Response(education, mimetype="application/json", status=200)
 
+    # GET all
     def index(self):
-        return render_template('education_documentation.html')
+        educations = Education.objects()
 
-    def create(self):
+        def convert_snake(string):
+            return '_'.join(
+                sub('([A-Z][a-z]+)', r' \1',
+                sub('([A-Z]+)', r' \1',
+                string.replace('-', ' '))).split()).lower()
+
+        get_all_education_objects = {}
+
+        for e in educations:
+            get_all_education_objects[convert_snake(e.classification)] = {
+                "type": str(e),
+                "id": str(e.id),
+                "attributes": {
+                    "classification": e.classification,
+                    "question": e.question,
+                    "description": e.description,
+                    "information": e.information,
+                    "note": e.note,
+                    "source": e.source
+                }
+            }
+
+        return {"data": get_all_education_objects}, 200
+
+    # POST
+    def add_education(self):
         # try:
         body = request.get_json()
         education = Education(**body) #will require auth later
@@ -23,4 +52,18 @@ class EducationController():
         #     raise EducationAlreadyExistsError
         # except Exception:
         #     raise InternalServerError
+
+    # PUT
+    def update_education(self, id):
+        education = Education.objects.get(id=id)
+        body = request.get_json()
+        education.update(**body)
+        return {"id": str(id), "to_see_update": "Run GET /api/v1/education or GET /api/v1/<id>"}, 200
+
+    # DESTROY
+    def destroy_education(self, id):
+        education = Education.objects.get(id=id)
+        education.delete()
+        return {"id": 'nil', "to_see_removal": "Run GET /api/v1/education or GET /api/v1/<id>"}, 200
+
 educationcontroller = EducationController()
