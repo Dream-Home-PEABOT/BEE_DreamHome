84539ec8e94e170fe826e7818d0ba7ac1ce75669
Priya Power <49959312+priyapower@users.noreply.github.com>
2021-01-07 13:41:41 -0700

    Update education test for placeholders and remove excess information

--- tests/test_education_crud.py
+++ tests/test_education_crud.py
@@ -2,21 +2,14 @@ import json
 from tests.BaseCase import BaseCase
 
 class TestEducationCrud(BaseCase):
+    # GET single
+
+    # GET all
+
+    # POST
     def test_post_education_response(self):
         # Given
-            # IN CASE WE NEED AUTHORIZATION (don't forget to update for our admin user):
-            # email = "paurakh011@gmail.com"
-            # password = "mycoolpassword"
-            # user_payload = json.dumps({
-            #     "email": email,
-            #     "password": password
-            # })
-            #
-            # response = self.app.post('/api/v1/auth/signup', headers={"Content-Type": "application/json"}, data=user_payload)
-            # user_id = response.json['id']
-            # response = self.app.post('/api/v1/auth/login', headers={"Content-Type": "application/json"}, data=user_payload)
-            # login_token = response.json['token']
-        education_survey_payload = {
+        payload = {
             "classification": "Annual Salary",
             "question": "What is your net monthly salary?",
             "description": "Gross income is the total amount you earn (typically over the course of a year) before expenses. Net income is the profit your business earns after expenses ",
@@ -26,7 +19,11 @@ class TestEducationCrud(BaseCase):
         }
 
         # When
-        response = self.app.post('/api/v1/education', headers={"Content-Type": "application/json"}, data=json.dumps(education_survey_payload))
+        response = self.app.post('/api/v1/education', headers={"Content-Type": "application/json"}, data=json.dumps(payload))
 
         # Then
         self.assertEqual(200, response.status_code)
+
+    # PUT
+
+    # DESTROY
