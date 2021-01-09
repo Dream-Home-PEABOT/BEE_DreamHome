873988371a6649b38423e76081779232c9ea0c37
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-09 12:26:59 -0600

    Adds CRUD functionality to Report Controller.
    Co-Authored-By: Priya Power <49959312+priyapower@users.noreply.github.com>

--- api/controller/ReportController.py
+++ api/controller/ReportController.py
@@ -1,35 +1,77 @@
-from flask import Response, request
+from flask import Response, request, render_template
 from api.models.report import Report
 from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist, ValidationError, InvalidQueryError
-from flask import render_template
 import pry
 
 class ReportController():
+    # GET single
+    def get_report(self, id):
+        report = Report.objects.get(id=id)
+        return {
+            "data": {
+                "type": str(report),
+                "id": str(report.id),
+                "attributes": {
+                    "input": {
+                        "salary": report.salary,
+                        "zipcode": report.zipcode,
+                        "credit_score": report.credit_score,
+                        "monthly_debt": report.monthly_debt,
+                        "downpayment_savings": report.downpayment_savings,
+                        "downpayment_percentage": report.downpayment_percentage,
+                        "rent": report.rent,
+                        "goal_principal": report.goal_principal,
+
+                    },
+                    "output": {}
+                }
+            }
+        }, 200
+
+
+    # POST
     def add_report(self):
         body = request.get_json()
         # make the report exist
         report = Report(**body)
         report.save()
         id = report.id
-        # have it return an
+        # have it return an id
         return {
             "data": {
                 "id": str(id),
                 "confirmation": {
                     "info": 'To connect this report to a user in the future, save this url with the client',
-                    "url": f'/api/v1/record/{id}'
+                    "url": f'/api/v1/report/{id}'
                 }
             }
         }, 200
 
-# GET single
-    def get_report(self, id):
+    def update_report(self, id):
         report = Report.objects.get(id=id)
+        body = request.get_json()
+        report.update(**body)
         return {
             "data": {
-                "type": string(report),
-                "id": str(report.id)
+                "id": str(id),
+                "confirmation": {
+                    "info": "To see this record's update response, please do a GET request using the url",
+                    "url": f'/api/v1/report/{id}'
+                }
             }
-        }
+        }, 200
 
-# GET all
+    def destroy_report(self, id):
+        report = Report.objects.get(id=id)
+        report.delete()
+        return {
+            "data": {
+                "id": 'nil',
+                "confirmation": {
+                    "info": "To see this record's deletion response, please do a GET request using the url",
+                    "url": f'/api/v1/education/{id}'
+                }
+            }
+        }, 200
+
+reportcontroller = ReportController()
