9b72b278ab7eb922cea050d10e571b64b3120e14
Priya Power <49959312+priyapower@users.noreply.github.com>
2021-01-11 10:44:28 -0700

    Add custom error to app errorhandler
    - Add the APIError Class to the errorhandler
    - The function returns a json response that renders the Sub Class 
    description and the custom message defined in the controller

--- dreamhome.py
+++ dreamhome.py
@@ -1,17 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
 from database.db import initialize_db
 from api.routes.api_routes import api
+from api.helpers.errors import APIError
+import traceback
+import pry
 
 app = Flask(__name__, template_folder = 'api/views')
 
 app.config.from_envvar('ENV_FILE_LOCATION')
-
-initialize_db(app)
 
 @app.route('/')
 def hello():
     return 'Welcome to your dream home server!'
 
 app.register_blueprint(api)
+initialize_db(app)
+
+@app.errorhandler(APIError)
+def handle_exception(err):
+    """Return custom JSON when APIError or its children are raised"""
+    response = {
+        "data": {
+            "error": err.description,
+            "message": ""
+        }
+    }
+    if len(err.args) > 0:
+        response["data"]["message"] = err.args[0]
+    # Add some logging so that we can monitor different types of errors
+    app.logger.error(f"{err.description}: {response['data']['message']}")
+    return jsonify(response), err.code
 
 app.run()
