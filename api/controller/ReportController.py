2596db1e4e6ea01727607ca6e0f7155da6bd79e2
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-12 11:01:29 -0600

    Adds error path to report controller.

--- api/controller/ReportController.py
+++ api/controller/ReportController.py
@@ -1,36 +1,42 @@
 from flask import Response, request, render_template
 from api.models.report import Report
 from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist, ValidationError, InvalidQueryError
+from api.helpers.errors import APINotUniqueError, APISchemaError, APIDoesNotExistError
 import pry
 
 class ReportController():
     # GET single
     def get_report(self, id):
-        report = Report.objects.get(id=id)
-        return {
-            "data": {
-                "type": str(report),
-                "id": str(report.id),
-                "attributes": {
-                    "input": {
-                        "salary": report.salary,
-                        "zipcode": report.zipcode,
-                        "credit_score": report.credit_score,
-                        "monthly_debt": report.monthly_debt,
-                        "downpayment_savings": report.downpayment_savings,
-                        "downpayment_percentage": report.downpayment_percentage,
-                        "rent": report.rent,
-                        "goal_principal": report.goal_principal,
+        try:
+            report = Report.objects.get(id=id)
+            return {
+                "data": {
+                    "type": str(report),
+                    "id": str(report.id),
+                    "attributes": {
+                        "input": {
+                            "salary": report.salary,
+                            "zipcode": report.zipcode,
+                            "credit_score": report.credit_score,
+                            "monthly_debt": report.monthly_debt,
+                            "downpayment_savings": report.downpayment_savings,
+                            "downpayment_percentage": report.downpayment_percentage,
+                            "rent": report.rent,
+                            "goal_principal": report.goal_principal,
 
-                    },
-                    "output": {}
+                        },
+                        "output": {}
+                    }
                 }
-            }
-        }, 200
-
+            }, 200
+        except (ValidationError, DoesNotExist):
+            raise APIDoesNotExistError("Please check your request, the Report record given id doesn't exist")
+        except Exception:
+            raise 500
 
     # POST
     def add_report(self):
+        # try:
         body = request.get_json()
         # make the report exist
         report = Report(**body)
@@ -46,6 +52,8 @@ class ReportController():
                 }
             }
         }, 200
+        # except NotUniqueError:
+        #     raise APINotUniqueError("This report record's salary")
 
     def update_report(self, id):
         report = Report.objects.get(id=id)
