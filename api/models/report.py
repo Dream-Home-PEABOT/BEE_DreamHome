35b716da164056d8351b65f19a91888f5ddf1cff
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-13 16:46:17 -0600

    Adds initial data model methods
    Co-Authored-By: Priya Power <49959312+priyapower@users.noreply.github.com>
    Co-Authored-By: Eric Hale <59599782+EHale64@users.noreply.github.com>

--- api/models/report.py
+++ api/models/report.py
@@ -1,6 +1,7 @@
 import pry
 from database.db import db
 import math
+import datetime
 
 class Report(db.Document):
     #Initial Post from FrontEnd
@@ -13,33 +14,59 @@ class Report(db.Document):
     rent = db.IntField(default=0)
     goal_principal = db.IntField(default=0)
 
-    def principal_based_on_rent(self):
-        monthly = self.rent
-        percentage = self.downpayment_percentage / 100
-        annual_interest = self.mortgage_rate()
-        monthly_interest = annual_interest / 12
-        number_payments = 360
-        exponent = math.pow((1 + monthly_interest), number_payments)
-        numerator = (monthly) * (exponent - 1)
-        denomenator = (percentage) * (monthly_interest * exponent)
-        imaginative_principal = numerator / denomenator
-        pry()
-        return imaginative_principal
-
-    def monthly_principal(self):
-        pass
-
-
+    def number_payments(self):
+        return 360
 
     def true_monthly(self):
-        pass
+        tm = self.rent + self.home_insurance() + self.property_tax()
+        if self.downpayment_percentage < 20:
+            tm += self.pmi()
+        return tm
+
     def home_insurance(self):
-        pass
+        return 125
 
     def mortgage_rate(self):
         return 0.045
 
     def pmi(self):
-        pass
+        return 45
+
     def property_tax(self):
-        pass
+        return 100
+
+    def monthly_principal(self):
+        return self.rent
+
+    def percentage_saved_based_on_principal(self):
+        return self.downpayment_percentage #fix this!
+
+    def downpayment_goal_monthly_savings(self, year):
+        monthly_goal = self.downpayment_savings / (year * 12) #fix this!
+        return round(monthly_goal)
+
+    def downpayment_savings_goal_end_date(self, year):
+        now = datetime.datetime.now()
+        month = now.month
+        current_year = now.year
+        day = now.day
+        new_year = current_year + year
+        date = f'{month}/{day}/{new_year}'
+        return date
+
+
+    def principal_based_on_rent(self):
+        monthly = self.rent
+        percentage = self.downpayment_percentage / 100
+        annual_interest = self.mortgage_rate()
+        monthly_interest = annual_interest / 12
+        number_payments = self.number_payments()
+
+        exponent = math.pow((1 + monthly_interest), number_payments)
+
+        numerator = (monthly) * (exponent - 1)
+
+        denomenator = (percentage) * (monthly_interest * exponent)
+
+        imaginative_principal = numerator / denomenator
+        return round(imaginative_principal)
