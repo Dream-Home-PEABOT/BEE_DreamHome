2424a7d6e1f857c92b9ea6a627f1017d342e6dc5
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-13 10:05:23 -0600

    IT F$$%$#%*^ WORKS!
    - used flaskmongoengine's app.config 
    - hid confidential information in .env
    - installed python.env
    - confirmed successful post through postman AND atlas
    
    Co-Authored-By: Priya Power <49959312+priyapower@users.noreply.github.com>

--- database/db.py
+++ database/db.py
@@ -1,20 +1,6 @@
-# from flask_mongoengine import MongoEngine
-import pymongo
-import os
+from flask_mongoengine import MongoEngine
 
-uri = os.environ.get('MONGODB_URI')
-client = pymongo.MongoClient(uri)
-db = client['dreamhome']
+db = MongoEngine()
 
 def initialize_db(app):
     db.init_app(app)
-
-# stream = open(options.filename, 'r')
-# yamlData = yaml.load(stream)
-# jsonData = json.dumps(yamlData)
-# id = StringIO(jsonData)
-#
-# ...calling class, etc, then
-# self.appCollection.insert(me)
-
-# https://stackoverflow.com/questions/21712181/python-insert-yaml-in-mongodb
