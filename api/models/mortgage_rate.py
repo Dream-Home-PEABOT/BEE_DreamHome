5cd55c0e3cf0568c849e8e37df3fc797f3933098
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-26 08:39:59 -0600

    Adds credit score floor and credit score cieling as attributes.
    Co-Authored-By: Priya Power <49959312+priyapower@users.noreply.github.com>

--- api/models/mortgage_rate.py
+++ api/models/mortgage_rate.py
@@ -1,5 +1,6 @@
 from database.db import db
 
 class MortgageRate(db.document):
-    credit_score_range = db.StringField(required=True, unique=True)
+    credit_score_floor = db.StringField(required=True, unique=True)
+    credit_score_cieling = db.StringField(required=True, unique=True)
     rate = FloatField(required=True)
