925bb01052c179822e8c12042fa6bfed74131d34
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-27 15:16:17 -0600

    Adds 51 state annual average insurance rates for home insurance.

--- database/insurance.py
+++ database/insurance.py
@@ -14,54 +14,253 @@ postgressive = {
 }
 
 
-1	Oklahoma	$4,445	($2,140)	93%
-2	Kansas	$3,931	($1,626)	71%
-3	Florida	$3,643	($1,338)	58%
-4	Arkansas	$3,439	($1,134)	49%
-5	Texas	$3,429	($1,124)	49%
-6	Mississippi	$3,340	($1,035)	45%
-7	Louisiana	$3,270	($965)	42%
-8	South Dakota	$3,172	($867)	38%
-9	Nebraska	$3,133	($828)	36%
-10	Missouri	$3,111	($806)	35%
-11	Colorado	$3,082	($777)	34%
-12	Minnesota	$3,010	($705)	31%
-13	Alabama	$2,981	($676)	29%
-14	Kentucky	$2,862	($557)	24%
-15	Montana	$2,809	($504)	22%
-16	Tennessee	$2,692	($387)	17%
-17	South Carolina	$2,678	($373)	16%
-18	North Dakota	$2,601	($296)	13%
-19	Georgia	$2,555	($250)	11%
-20	Iowa	$2,540	($235)	10%
-21	West Virginia	$2,486	($181)	8%
-22	Indiana	$2,423	($118)	5%
-23	New Mexico	$2,299	$6	0%
-24	Illinois	$2,201	$104	-5%
-25	Michigan	$2,153	$152	-7%
-26	Rhode Island	$2,125	$180	-8%
-27	Ohio	$2,107	$198	-9%
-28	Wyoming	$2,083	$222	-10%
-29	North Carolina	$2,009	$296	-13%
-30	Arizona	$1,976	$329	-14%
-31	Connecticut	$1,961	$344	-15%
-32	Virginia	$1,956	$349	-15%
-33	Massachusetts  	$1,920	$385	-17%
-34	Idaho	$1,842	$463	-20%
-35	New York	$1,840	$465	-20%
-36	Maine	$1,833	$472	-20%
-37	Alaska	$1,799	$506	-22%
-38	New Jersey	$1,744	$561	-24%
-39	Wisconsin	$1,732	$573	-25%
-40	Pennsylvania	$1,720	$585	-25%
-41	Oregon	$1,608	$697	-30%
-42	Delaware	$1,521	$784	-34%
-43	Maryland	$1,518	$787	-34%
-44	Washington	$1,514	$791	-34%
-45	DC	$1,488	$817	-35%
-46	Nevada	$1,486	$819	-36%
-47	New Hampshire	$1,455	$850	-37%
-48	Utah	$1,378	$927	-40%
-49	Vermont	$1,212	$1,093	-47%
-50	California	$1,166	$1,139	-49%
-51	Hawaii	$499	$1,806	-78%
+oklahoma = {
+    "state": "Oklahoma",
+    "annual_average_insurance_rate": 4445
+}
+
+kansas = {
+    "state": "Kansas",
+    "annual_average_insurance_rate": 3931
+}
+
+florida = {
+    "state": "Florida",
+    "annual_average_insurance_rate": 3643
+}
+
+arkansas = {
+    "state": "Arkansas",
+    "annual_average_insurance_rate": 3439
+}
+
+texas = {
+    "state": "Texas",
+    "annual_average_insurance_rate": 3429
+}
+
+mississippi = {
+    "state": "Mississippi",
+    "annual_average_insurance_rate": 3340
+}
+
+louisiana = {
+    "state": "Louisiana",
+    "annual_average_insurance_rate": 3270
+}
+
+south_dakota = {
+    "state": "South Dakota",
+    "annual_average_insurance_rate": 3172
+}
+
+nebraska = {
+    "state": "Nebraska",
+    "annual_average_insurance_rate": 3133
+}
+
+missouri = {
+    "state": "Missouri",
+    "annual_average_insurance_rate": 3111
+}
+
+colorado = {
+    "state": "Colorado",
+    "annual_average_insurance_rate": 3082
+}
+
+minnesota = {
+    "state": "Minnesota",
+    "annual_average_insurance_rate": 3010
+}
+
+alabama = {
+    "state": "Alabama",
+    "annual_average_insurance_rate": 2981
+}
+
+kentucky = {
+    "state": "Kentucky",
+    "annual_average_insurance_rate": 2862
+}
+
+montana = {
+    "state": "Montana",
+    "annual_average_insurance_rate": 2809
+}
+
+tennessee = {
+    "state": "Tennessee",
+    "annual_average_insurance_rate": 2692
+}
+
+south_carolina = {
+    "state": "South Carolina",
+    "annual_average_insurance_rate": 2678
+}
+
+north_dakota = {
+    "state": "North Dakota",
+    "annual_average_insurance_rate": 2601
+}
+
+georgia = {
+    "state": "Georgia",
+    "annual_average_insurance_rate": 2555
+}
+
+iowa = {
+    "state": "Iowa",
+    "annual_average_insurance_rate": 2540
+}
+
+west_virginia = {
+    "state": "West Virginia",
+    "annual_average_insurance_rate": 2486
+}
+
+indiana = {
+    "state": "Indiana",
+    "annual_average_insurance_rate": 2423
+}
+
+new_mexico = {
+    "state": "New Mexico",
+    "annual_average_insurance_rate": 2299
+}
+
+illinois = {
+    "state": "Illnois",
+    "annual_average_insurance_rate": 2201
+}
+michigan = {
+    "state": "Michigan",
+    "annual_average_insurance_rate": 2153
+}
+rhode_island = {
+    "state": "Rhode Island",
+    "annual_average_insurance_rate": 2125
+}
+
+ohio = {
+    "state": "Ohio",
+    "annual_average_insurance_rate": 2107
+}
+
+wyoming = {
+    "state": "Wyoming",
+    "annual_average_insurance_rate": 2083
+}
+north_carolina = {
+    "state": "North Carolina",
+    "annual_average_insurance_rate": 2009
+}
+arizona = {
+    "state": "Arizona",
+    "annual_average_insurance_rate": 1976
+}
+
+connecticut = {
+    "state": "Connecticut",
+    "annual_average_insurance_rate": 1961
+}
+
+virginia = {
+    "state": "Virginia",
+    "annual_average_insurance_rate": 1956
+}
+
+massachursetts = {
+    "state": "Massachusetts",
+    "annual_average_insurance_rate": 1920
+}
+
+idaho = {
+    "state": "Idaho",
+    "annual_average_insurance_rate": 1842
+}
+
+new_york = {
+    "state": "New York",
+    "annual_average_insurance_rate": 1840
+}
+
+maine = {
+    "state": "Maine",
+    "annual_average_insurance_rate": 1833
+}
+
+alaska = {
+    "state": "Alaska",
+    "annual_average_insurance_rate": 1799
+}
+
+new_jersey = {
+    "state": "New Jersey",
+    "annual_average_insurance_rate": 1744
+}
+
+wisconsin = {
+    "state": "Wisconsin",
+    "annual_average_insurance_rate": 1732
+}
+
+pennsylvania = {
+    "state": "Pennsylvania",
+    "annual_average_insurance_rate": 1720
+}
+
+oregon = {
+    "state": "Oregon",
+    "annual_average_insurance_rate": 1608
+}
+
+deleware = {
+    "state": "Delaware",
+    "annual_average_insurance_rate": 1521
+}
+
+maryland = {
+    "state": "Maryland",
+    "annual_average_insurance_rate": 1518
+}
+
+washington = {
+    "state": "Washington",
+    "annual_average_insurance_rate": 1514
+}
+
+dc = {
+    "state": "DC",
+    "annual_average_insurance_rate": 1488
+}
+
+nevada = {
+    "state": "Nevada",
+    "annual_average_insurance_rate": 1486
+}
+
+new_hampshire = {
+    "state": "New Hampshire",
+    "annual_average_insurance_rate": 1455
+}
+
+utah = {
+    "state": "Utah",
+    "annual_average_insurance_rate": 1378
+}
+
+vermont = {
+    "state": "Vermont",
+    "annual_average_insurance_rate": 1212
+}
+
+california = {
+    "state": "California",
+    "annual_average_insurance_rate": 1166
+}
+
+hawaii = {
+    "state": "Hawaii",
+    "annual_average_insurance_rate": 499
+}
