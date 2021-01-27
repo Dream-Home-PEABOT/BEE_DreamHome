4d2dc51f2da7e0c9a50043b1c1c64b38b460149d
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-26 22:16:59 -0600

    Adds required fields for testing crud functionality of mortgage rate.

---
+++ tests/test_mortgage_rate_crud.py
@@ -0,0 +1,4 @@
+import pry
+import json
+from tests.BaseCase import BaseCase
+from database.mortgage_rate import range_620_639, range_640_659, range_660_679, range_680_699, range_700_759, range_760_850
