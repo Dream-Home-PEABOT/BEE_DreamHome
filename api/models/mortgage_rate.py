9b03742a19a423390a084179019278d96292c07c
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-26 23:57:06 -0600

    Cleans up bugs in mortgage rate model.

--- api/models/mortgage_rate.py
+++ api/models/mortgage_rate.py
@@ -1,6 +1,6 @@
 from database.db import db
 
-class MortgageRate(db.document):
+class MortgageRate(db.Document):
     credit_score_floor = db.StringField(required=True, unique=True)
     credit_score_cieling = db.StringField(required=True, unique=True)
-    rate = FloatField(required=True)
+    rate = db.FloatField(required=True)
