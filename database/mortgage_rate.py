6158ad51f647040f9369582364d8c2686ee35d40
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-26 22:17:11 -0600

    Creates all seeds for mortgage_rate

---
+++ database/mortgage_rate.py
@@ -0,0 +1,37 @@
+# range_300_619 = {
+#     "credit_score_floor": "620",
+#     "credit_score_cieling": "639",
+#     "rate": 
+# "This one is special": "And needs something informational"
+# }
+
+range_620_639 = {
+    "credit_score_floor": "620",
+    "credit_score_cieling":"639",
+    "rate": 4.13
+}
+range_640_659 = {
+    "credit_score_floor": "640",
+    "credit_score_cieling": "659",
+    "rate": 3.58
+}
+range_660_679 = {
+    "credit_score_floor": "660",
+    "credit_score_cieling": "679",
+    "rate": 3.15
+}
+range_680_699 = {
+    "credit_score_floor": "680",
+    "credit_score_cieling": "699",
+    "rate": 2.945
+}
+range_700_759 = {
+    "credit_score_floor": "700",
+    "credit_score_cieling": "759",
+    "rate": 2.76
+}
+range_760_850 = {
+    "credit_score_floor": "760",
+    "credit_score_cieling": "850",
+    "rate": 2.54
+}
