f898af907c36a27a10b9bd5eb3cc87663e748eec
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-28 15:23:39 -0600

    Adds sadpath testing to getting report by uid
    Co-Authored-By: Priya Power <49959312+priyapower@users.noreply.github.com>

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -55,24 +55,87 @@ class TestAuthorization(BaseCase):
         self.assertNotEqual(harry_report['credit_score'], ginny_data['03_attributes']['input']['B_credit_score'])
 
 
+    def test_user_wants_to_register(self):
+    # Given
+        unregistered_report = {
+            "zipcode": 80209,
+            "credit_score": 710,
+            "salary": 5000,
+            "monthly_debt": 1500,
+            "downpayment_savings": 50000,
+            "mortgage_term": 30,
+            "downpayment_percentage": 20,
+            "goal_principal": 500000,
+            "rent": 0
+        }
 
-    #unregistered user w/ report that registers AND UID ADDED
-    #Given
+        response = self.app.post('/api/v1/report', headers={"Content-Type": "application/json"}, data=json.dumps(unregistered_report))
+        data = response.json['data']
+        id = data['id']
+        url = data['confirmation']['url']
+        unregistered_confirmation = self.app.get(url, headers={"Content-Type": "application/json"})
+    # When
+        uid = 'asefiojoidfcjcoiaswe4freowi4'
 
-    # unregistered user has already created a report w/o a uid
-    #     - generate correct report
-    #     - generate false report
-    #
-    # unregistered user registers
-    #     -  local variable uid
-    #
-    # can put a uid to a correct report
-    #
-    # confirm correct report has uid and not false report
-    # confirm using new route that will automatically search by uid api/v1/report/unique
-    #     - find report matched to the uid
-    #     - otherwise it'll return an error
-    #         - this user does not have a report yet/assigned
+        user_payload = {
+            "uid": uid
+        }
 
-    #When
+        updated_response = self.app.put(f'/api/v1/report/{id}', headers={"Content-Type": "application/json"}, data=json.dumps(user_payload))
+        updated_data = updated_response.json['data']
+
+        registered_confirmation = self.app.get('/api/v1/report/unique', headers={"Content-Type": "application/json"}, data=json.dumps(uid))
     #Then
+        self.assertEqual(unregistered_confirmation.json, registered_confirmation.json)
+
+    def test_user_wants_to_register(self):
+    # Given
+        unregistered_report = {
+            "zipcode": 80209,
+            "credit_score": 710,
+            "salary": 5000,
+            "monthly_debt": 1500,
+            "downpayment_savings": 50000,
+            "mortgage_term": 30,
+            "downpayment_percentage": 20,
+            "goal_principal": 500000,
+            "rent": 0
+        }
+
+        response = self.app.post('/api/v1/report', headers={"Content-Type": "application/json"}, data=json.dumps(unregistered_report))
+
+    # When
+
+
+        error_confirmation = self.app.get('/api/v1/report/unique', headers={"Content-Type": "application/json"})
+    #Then
+        data = error_confirmation.json['data']
+        self.assertEqual(410, data['error'])
+        self.assertEqual("Missing UID", data['message'])
+
+    def test_registered_user_wrong_uid(self):
+    # Given
+        registered_report = {
+            "zipcode": 80209,
+            "credit_score": 710,
+            "salary": 5000,
+            "monthly_debt": 1500,
+            "downpayment_savings": 50000,
+            "mortgage_term": 30,
+            "downpayment_percentage": 20,
+            "goal_principal": 500000,
+            "rent": 0,
+            "uid": 'wsorefijcocetijvspowq2p'
+        }
+
+        response = self.app.post('/api/v1/report', headers={"Content-Type": "application/json"}, data=json.dumps(registered_report))
+
+    # When
+        wrong_uid = 'awdorfnjcaoielruwbn'
+
+        error_confirmation = self.app.get('/api/v1/report/unique', headers={"Content-Type": "application/json"}, data=json.dumps(wrong_uid))
+    #Then
+        data = error_confirmation.json['data']
+        self.assertEqual(406, data['error'])
+        self.assertEqual("No report matches this user", data['message'])
+# pass w/ wrong uid
