d652b03a68f0d54f5c892bc22a9ddcff53cc3abd
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-29 20:31:45 -0600

    Seed file incomplete
    Co-Authored-By: Priya Power <49959312+priyapower@users.noreply.github.com>

--- database/median_home_value.py
+++ database/median_home_value.py
@@ -7,6 +7,7 @@ alabama = {
     "median_bottom_tier": 68371,
     "median_condo_value": 198697
 }
+
 alaska = {
     "year": 2020,
     "state": "Alaska",
@@ -16,6 +17,7 @@ alaska = {
     "median_bottom_tier": 191152,
     "median_condo_value": 206374
 }
+
 arizona = {
     "year": 2020,
     "state": "Arizona",
@@ -25,6 +27,7 @@ arizona = {
     "median_bottom_tier": 197259,
     "median_condo_value": 189677
 }
+
 arkansas = {
     "year": 2020,
     "state": "Arkansas",
@@ -34,6 +37,7 @@ arkansas = {
     "median_bottom_tier": 64097,
     "median_condo_value": 152569
 }
+
 california = {
     "year": 2020,
     "state": "California",
@@ -43,6 +47,7 @@ california = {
     "median_bottom_tier": 588050,
     "median_condo_value": 525594
 }
+
 colorado = {
     "year": 2020,
     "state": "Colorado",
@@ -52,6 +57,7 @@ colorado = {
     "median_bottom_tier": 279396,
     "median_condo_value": 315402
 }
+
 connecticut = {
     "year": 2020,
     "state": "Connecticut",
@@ -61,6 +67,7 @@ connecticut = {
     "median_bottom_tier": 170615,
     "median_condo_value": 164659
 }
+
 delaware = {
     "year": 2020,
     "state": "Delaware",
@@ -70,6 +77,7 @@ delaware = {
     "median_bottom_tier": 179496,
     "median_condo_value": 214201
 }
+
 florida = {
     "year": 2020,
     "state": "Florida",
@@ -79,6 +87,52 @@ florida = {
     "median_bottom_tier": 158180,
     "median_condo_value": 194264
 }
+# "Georgia"
+georgia = {
+    "year": 2020,
+    "state": "Georgia",
+    "avg_home_value": 208833,
+    "median_top_tier": 365322,
+    "median_single_family_value": 208466,
+    "median_bottom_tier": 117107,
+    "median_condo_value": 216612
+}
+#     31. Average home value in Georgia: $208833
+#     Median top-tier home value in 2020: $365322
+#     Median single-family house value in 2020: $208466
+#     Median bottom-tier home value in 2020: $117107
+#     Median condo value in 2020: $216612
+# "Hawaii"
+hawaii = {
+    "year": 2020,
+    "state": "Hawaii",
+    "avg_home_value": 646733,
+    "median_top_tier": 1003485,
+    "median_single_family_value": 727931,
+    "median_bottom_tier": 380055,
+    "median_condo_value": 459264
+}
+#     1. Average home value in Hawaii: $646733
+#     Median top-tier home value in 2020: $1003485
+#     Median single-family house value in 2020: $727931
+#     Median bottom-tier home value in 2020: $380055
+#     Median condo value in 2020: $459264
+# "Idaho"
+idaho = {
+    "year": 2020,
+    "state": "Idaho",
+    "avg_home_value": 303606,
+    "median_top_tier": 464079,
+    "median_single_family_value": 304036,
+    "median_bottom_tier": 205803,
+    "median_condo_value": 281977
+}
+#     14. Average home value in Idaho: $303606
+#     Median top-tier home value in 2020: $464079
+#     Median single-family house value in 2020: $304036
+#     Median bottom-tier home value in 2020: $205803
+#     Median condo value in 2020: $281977
+
 illinois = {
     "year": 2020,
     "state": "Illinois",
@@ -88,3 +142,298 @@ illinois = {
     "median_bottom_tier": 105910,
     "median_condo_value": 201587
 }
+
+# "Indiana"
+indiana = {
+    "year": 2020,
+    "state": "Indiana",
+    "avg_home_value": 161106,
+    "median_top_tier": 271339,
+    "median_single_family_value": 161291,
+    "median_bottom_tier": 88260,
+    "median_condo_value": 156609
+}
+#     41. Average home value in Indiana: $161106
+#     Median top-tier home value in 2020: $271339
+#     Median single-family house value in 2020: $161291
+#     Median bottom-tier home value in 2020: $88260
+#     Median condo value in 2020: $156609
+# "Iowa"
+iowa = {
+    "year": 2020,
+    "state": "Iowa",
+    "avg_home_value": 155781,
+    "median_top_tier": 265520,
+    "median_single_family_value": 157261,
+    "median_bottom_tier": 83788,
+    "median_condo_value": 137156
+}
+#     43. Average home value in Iowa: $155781
+#     Median top-tier home value in 2020: $265520
+#     Median single-family house value in 2020: $157261
+#     Median bottom-tier home value in 2020: $83788
+#     Median condo value in 2020: $137156
+# "Kansas"
+kansas = {
+    "year": 2020,
+    "state": "Kansas",
+    "avg_home_value": 154531,
+    "median_top_tier": 287863,
+    "median_single_family_value": 154777,
+    "median_bottom_tier": 71887,
+    "median_condo_value": 135736
+}
+#     44. Average home value in Kansas: $154531
+#     Median top-tier home value in 2020: $287863
+#     Median single-family house value in 2020: $154777
+#     Median bottom-tier home value in 2020: $71887
+#     Median condo value in 2020: $135736
+# "Kentucky"
+kentucky = {
+    "year": 2020,
+    "state": "Kentucky",
+    "avg_home_value": 150918,
+    "median_top_tier": 261262,
+    "median_single_family_value": 150877,
+    "median_bottom_tier": 77463,
+    "median_condo_value": 150920
+}
+#     45. Average home value in Kentucky: $150918
+#     Median top-tier home value in 2020: $261262
+#     Median single-family house value in 2020: $150877
+#     Median bottom-tier home value in 2020: $77463
+#     Median condo value in 2020: $150920
+# "Louisiana"
+louisiana = {
+    "year": 2020,
+    "state": "Louisiana",
+    "avg_home_value": 171485,
+    "median_top_tier": 296599,
+    "median_single_family_value": 171948,
+    "median_bottom_tier": 94364,
+    "median_condo_value": 153199
+}
+#     39. Average home value in Louisiana: $171485
+#     Median top-tier home value in 2020: $296599
+#     Median single-family house value in 2020: $171948
+#     Median bottom-tier home value in 2020: $94364
+#     Median condo value in 2020: $153199
+# "Maine"
+maine = {
+    "year": 2020,
+    "state": "Maine",
+    "avg_home_value": 253914,
+    "median_top_tier": 422248,
+    "median_single_family_value": 252014,
+    "median_bottom_tier": 150408,
+    "median_condo_value": 278898
+}
+#     25. Average home value in Maine: $253914
+#     Median top-tier home value in 2020: $422248
+#     Median single-family house value in 2020: $252014
+#     Median bottom-tier home value in 2020: $150408
+#     Median condo value in 2020: $278898
+# "Maryland"
+maryland = {
+    "year": 2020,
+    "state": "Maryland",
+    "avg_home_value": 317033,
+    "median_top_tier": 513094,
+    "median_single_family_value": 327604,
+    "median_bottom_tier": 192725,
+    "median_condo_value": 224299
+}
+#     10. Average home value in Maryland: $317033
+#     Median top-tier home value in 2020: $513094
+#     Median single-family house value in 2020: $327604
+#     Median bottom-tier home value in 2020: $192725
+#     Median condo value in 2020: $224299
+# "Massachusetts"
+massachusetts = {
+    "year": 2020,
+    "state": "Massachusetts",
+    "avg_home_value": 439541,
+    "median_top_tier": 732598,
+    "median_single_family_value": 447367,
+    "median_bottom_tier": 283343,
+    "median_condo_value": 393196
+}
+#     3. Average home value in Massachusetts: $439541
+#     Median top-tier home value in 2020: $732598
+#     Median single-family house value in 2020: $447367
+#     Median bottom-tier home value in 2020: $283343
+#     Median condo value in 2020: $393196
+# "Michigan"
+#     37. Average home value in Michigan: $180404
+#     Median top-tier home value in 2020: $313720
+#     Median single-family house value in 2020: $177403
+#     Median bottom-tier home value in 2020: $90535
+#     Median condo value in 2020: $211352
+# "Minnesota"
+#     19. Average home value in Minnesota: $266887
+#     Median top-tier home value in 2020: $403999
+#     Median single-family house value in 2020: $270308
+#     Median bottom-tier home value in 2020: $169614
+#     Median condo value in 2020: $191041
+# "Mississippi"
+#     49. Average home value in Mississippi: $126511
+#     Median top-tier home value in 2020: $223621
+#     Median single-family house value in 2020: $126294
+#     Median bottom-tier home value in 2020: $68093
+#     Median condo value in 2020: $145414
+# "Missouri"
+#     40. Average home value in Missouri: $167700
+#     Median top-tier home value in 2020: $288175
+#     Median single-family house value in 2020: $168392
+#     Median bottom-tier home value in 2020: $90857
+#     Median condo value in 2020: $155283
+# "Montana"
+#     17. Average home value in Montana: $293685
+#     Median top-tier home value in 2020: $464744
+#     Median single-family house value in 2020: $293244
+#     Median bottom-tier home value in 2020: $185975
+#     Median condo value in 2020: $305874
+# "Nebraska"
+#     38. Average home value in Nebraska: $180397
+#     Median top-tier home value in 2020: $291471
+#     Median single-family house value in 2020: $180496
+#     Median bottom-tier home value in 2020: $104964
+#     Median condo value in 2020: $174687
+# "Nevada"
+#     12. Average home value in Nevada: $311018
+#     Median top-tier home value in 2020: $458950
+#     Median single-family house value in 2020: $322809
+#     Median bottom-tier home value in 2020: $223577
+#     Median condo value in 2020: $168460
+# "New Hampshire"
+#     13. Average home value in New Hampshire: $303875
+#     Median top-tier home value in 2020: $456232
+#     Median single-family house value in 2020: $315079
+#     Median bottom-tier home value in 2020: $199074
+#     Median condo value in 2020: $231709
+# "New Jersey"
+#     8. Average home value in New Jersey: $346368
+#     Median top-tier home value in 2020: $584459
+#     Median single-family house value in 2020: $355423
+#     Median bottom-tier home value in 2020: $205231
+#     Median condo value in 2020: $275174
+# "New Mexico"
+#     28. Average home value in New Mexico: $214213
+#     Median top-tier home value in 2020: $358768
+#     Median single-family house value in 2020: $215378
+#     Median bottom-tier home value in 2020: $142717
+#     Median condo value in 2020: $180527
+# "New York"
+#     9. Average home value in New York: $331459
+#     Median top-tier home value in 2020: $728940
+#     Median single-family house value in 2020: $275426
+#     Median bottom-tier home value in 2020: $145097
+#     Median condo value in 2020: $593067
+# "North Carolina"
+#     30. Average home value in North Carolina: $210766
+#     Median top-tier home value in 2020: $366161
+#     Median single-family house value in 2020: $212247
+#     Median bottom-tier home value in 2020: $115378
+#     Median condo value in 2020: $172189
+# "North Dakota"
+#     26. Average home value in North Dakota: $236405
+#     Median top-tier home value in 2020: $347710
+#     Median single-family house value in 2020: $241803
+#     Median bottom-tier home value in 2020: $155757
+#     Median condo value in 2020: $154687
+# "Ohio"
+#     42. Average home value in Ohio: $156343
+#     Median top-tier home value in 2020: $275136
+#     Median single-family house value in 2020: $157386
+#     Median bottom-tier home value in 2020: $83321
+#     Median condo value in 2020: $144814
+# "Oklahoma"
+#     47. Average home value in Oklahoma: $134289
+#     Median top-tier home value in 2020: $235037
+#     Median single-family house value in 2020: $134995
+#     Median bottom-tier home value in 2020: $64895
+#     Median condo value in 2020: $83881
+# "Oregon"
+#     6. Average home value in Oregon: $374604
+#     Median top-tier home value in 2020: $550478
+#     Median single-family house value in 2020: $377275
+#     Median bottom-tier home value in 2020: $263013
+#     Median condo value in 2020: $302959
+# "Pennsylvania"
+#     34. Average home value in Pennsylvania: $200367
+#     Median top-tier home value in 2020: $351542
+#     Median single-family house value in 2020: $201300
+#     Median bottom-tier home value in 2020: $108661
+#     Median condo value in 2020: $185901
+# "Rhode Island"
+#     11. Average home value in Rhode Island: $312255
+#     Median top-tier home value in 2020: $495252
+#     Median single-family house value in 2020: $317918
+#     Median bottom-tier home value in 2020: $236324
+#     Median condo value in 2020: $246972
+# "South Carolina"
+#     35. Average home value in South Carolina: $193491
+#     Median top-tier home value in 2020: $346615
+#     Median single-family house value in 2020: $195761
+#     Median bottom-tier home value in 2020: $106900
+#     Median condo value in 2020: $165922
+# "South Dakota"
+#     27. Average home value in South Dakota: $217215
+#     Median top-tier home value in 2020: $334102
+#     Median single-family house value in 2020: $217789
+#     Median bottom-tier home value in 2020: $143611
+#     Median condo value in 2020: $169650
+# "Tennessee"
+#     36. Average home value in Tennessee: $192275
+#     Median top-tier home value in 2020: $338100
+#     Median single-family house value in 2020: $191164
+#     Median bottom-tier home value in 2020: $99130
+#     Median condo value in 2020: $215736
+# "Texas"
+#     29. Average home value in Texas: $213036
+#     Median top-tier home value in 2020: $367998
+#     Median single-family house value in 2020: $213666
+#     Median bottom-tier home value in 2020: $126071
+#     Median condo value in 2020: $185154
+# "Utah"
+#     7. Average home value in Utah: $360044
+#     Median top-tier home value in 2020: $540784
+#     Median single-family house value in 2020: $365960
+#     Median bottom-tier home value in 2020: $262743
+#     Median condo value in 2020: $300670
+# "Vermont"
+#     20. Average home value in Vermont: $264777
+#     Median top-tier home value in 2020: $404897
+#     Median single-family house value in 2020: $269754
+#     Median bottom-tier home value in 2020: $171962
+#     Median condo value in 2020: $235008
+# "Virginia"
+#     16. Average home value in Virginia: $293818
+#     Median top-tier home value in 2020: $547212
+#     Median single-family house value in 2020: $295926
+#     Median bottom-tier home value in 2020: $165069
+#     Median condo value in 2020: $278124
+# "Washington"
+#     4. Average home value in Washington: $431487
+#     Median top-tier home value in 2020: $723762
+#     Median bottom-tier home value in 2020: $289328
+#     Median single-family house value in 2020: $437442
+#     Median condo value in 2020: $368134
+# "West Virginia"
+#     50. Average home value in West Virginia: $107064
+#     Median top-tier home value in 2020: $199651
+#     Median single-family house value in 2020: $106840
+#     Median bottom-tier home value in 2020: $52166
+#     Median condo value in 2020: $126199
+# "Wisconsin"
+#     32. Average home value in Wisconsin: $206169
+#     Median top-tier home value in 2020: $330532
+#     Median single-family house value in 2020: $207906
+#     Median bottom-tier home value in 2020: $129163
+#     Median condo value in 2020: $188368
+# "Wyoming"
+#     23. Average home value in Wyoming: $254753
+#     Median top-tier home value in 2020: $412188
+#     Median single-family house value in 2020: $254065
+#     Median bottom-tier home value in 2020: $167987
+#     Median condo value in 2020: $325907
