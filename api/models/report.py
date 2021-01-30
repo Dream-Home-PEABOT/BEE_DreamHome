ab14e08a6ea63548c4ae48793ffcfb07af582266
Priya Power <49959312+priyapower@users.noreply.github.com>
2021-01-30 11:49:06 -0700

    Refactor Report Model and put guards for possible user edge cases
    - Guard against a user having a remaining_monthly cash pot of less than 
    500 (2020 minimum living standard estimate)
    - Guard against a user inputting a mortgage term of less than 5 (As of 
    2020, 5 is the minimum number of years you can ask for a mortgage term)
    - Guard against a principal calculated based on rent being below minimum 
    purchasing standards (As of 2020, the minimum value is 30000 accounting 
    for condo/townhome or mobile home prices in smaller cities)
    - Guard against a user having more savings than their choice in 
    downpayment percentage options. This will automatically Zero-Out the 
    downpayment savings portion
    - Refactors abbreviated state to full state name behavior
    - Refactors Credit Score to pmi and mortgage rates
    - Other minor refactorization for smaller function sizes and for 
    potential future detailed report calls

--- api/models/report.py
+++ api/models/report.py
@@ -21,101 +21,160 @@ class Report(db.Document):
     rent = db.IntField(default=0)
     uid = db.StringField()
 
-    def zip_to_avg_home(self, city_state):
+    # An IMAGINATIVE user does not have a goal principal in mind, but can give their current rental information to get an estimate of what they could afford by relating rent to monthly principal
+    # A PRAGMATIC user knows the goal principal they are seeking, therefore will not enter rent and all calculations are based on the goal principal they entered
+    # Helper Method for pmi && ReportController guard against goal principal vs rent
+    def imaginative_or_pragmatic(self):
+        if (self.goal_principal != 0) and (self.rent != 0 ):
+            self.rent = 0
+
+    # Helper method for monthly_principal && principal_based_on_rent
+    def number_payments(self):
+        if self.mortgage_term < 5:
+            self.mortgage_term = 5
+        return self.mortgage_term * 12
+
+    # Helper method for zip_to_avg_home && home_insurance && property_tax
+    def convert_abbreviation_to_full_state_name(self, city_state):
         abbrev_state = city_state[-2:]
         state = states[abbrev_state]
+        return state
+
+    # 1/5 Data Points for Report - BASED ON STATE
+    def zip_to_avg_home(self, city_state):
+        state = self.convert_abbreviation_to_full_state_name(city_state)
         median_home = MedianHomeValue.objects.get(state=state)
         return median_home.avg_home_value
 
-    def number_payments(self):
-        return self.mortgage_term * 12
-
+    # 2/5 Data Points for Report - BASED ON STATE
     def home_insurance(self, city_state):
-        zip = self.zipcode
-        grab_state = city_state[-2:]
-        state = states[grab_state]
+        state = self.convert_abbreviation_to_full_state_name(city_state)
         home_insurance = HomeInsurance.objects.get(state=state)
         monthly_insurance = home_insurance.annual_average_insurance_rate / 12
         return round(monthly_insurance)
 
-    def mortgage_rate(self):
-        report_cs = self.credit_score
-        if report_cs <= 619:
-            user_ceiling = "619"
-        elif report_cs in range(620, 640):
-            user_ceiling = "639"
-        elif report_cs in range(640, 660):
-            user_ceiling = "659"
-        elif report_cs in range(660, 680):
-            user_ceiling = "679"
-        elif report_cs in range(680, 700):
-            user_ceiling = "699"
-        elif report_cs in range(700, 760):
-            user_ceiling = "759"
-        elif report_cs in range(760, 851):
-            user_ceiling = "850"
-
-        mortgage_rate = MortgageRate.objects.get(credit_score_ceiling=user_ceiling)
-        rate = mortgage_rate.rate / 100
-        return round(rate, 4)
-
+    # 3/5 Data Points for Report - BASED ON STATE
     def property_tax(self, city_state):
-        zip = self.zipcode
-        grab_state = city_state[-2:]
-        state = states[grab_state]
+        state = self.convert_abbreviation_to_full_state_name(city_state)
         property_tax = PropertyTax.objects.get(state=state)
         monthly_property_tax = property_tax.annual_avg_property_tax / 12
         return round(monthly_property_tax)
 
+    # 4/5 Data Points for Report - BASED ON CREDIT SCORE
+    def mortgage_rate(self):
+        user_ceiling = self.credit_to_mortgage_range(self.credit_score)
+        mortgage_rate = MortgageRate.objects.get(credit_score_ceiling=user_ceiling)
+        rate = mortgage_rate.rate / 100
+        return round(rate, 4)
+
+    # 5/5 Data Points for Report - BASED ON DOWNPAYMENT PERCENTAGE USER SELECTS && CREDIT SCORE
     def pmi(self):
-        report_dp = self.downpayment_percentage
-        if report_dp <= 4:
+        guarded_dp = self.downpayment_percentage_to_db_pmi_object_range(self.downpayment_percentage)
+        if type(guarded_dp) == str:
+            return guarded_dp
+
+        principal = self.principal_imaginative_or_pragmatic()
+        pmi = Pmi.objects.get(downpayment_percentage=guarded_dp)
+        monthly_pmi = self.monthly_pmi_calculation(principal, pmi)
+        return round(monthly_pmi)
+
+    # Helper Method for mortgage_rate
+    def credit_to_mortgage_range(self, credit_score):
+        if credit_score <= 619:
+            user_ceiling = "619"
+        elif credit_score in range(620, 640):
+            user_ceiling = "639"
+        elif credit_score in range(640, 660):
+            user_ceiling = "659"
+        elif credit_score in range(660, 680):
+            user_ceiling = "679"
+        elif credit_score in range(680, 700):
+            user_ceiling = "699"
+        elif credit_score in range(700, 760):
+            user_ceiling = "759"
+        elif credit_score in range(760, 851):
+            user_ceiling = "850"
+        return user_ceiling
+
+    # Helper Method for pmi
+    def monthly_pmi_calculation(self, principal, pmi):
+        collector = self.credit_to_pmi_range(self.credit_score, pmi)
+        reduced_principal = (1 - (pmi.downpayment_percentage / 100)) * principal
+        annual_pmi = reduced_principal * (collector / 100)
+        monthly_pmi = (annual_pmi / 12)
+        return monthly_pmi
+
+    # Helper Method fo pmi
+    def downpayment_percentage_to_db_pmi_object_range(self, downpayment):
+        if downpayment <= 4:
             guarded_dp = 0
-        elif report_dp in range(5, 10):
+        elif downpayment in range(5, 10):
             guarded_dp = 5
-        elif report_dp in range(10, 15):
+        elif downpayment in range(10, 15):
             guarded_dp = 10
-        elif report_dp in range(15, 20):
+        elif downpayment in range(15, 20):
             guarded_dp = 15
         else:
             return "No PMI required for a 20 percent or higher downpayment."
+        return guarded_dp
 
-        report_cs = self.credit_score
-        pmi = Pmi.objects.get(downpayment_percentage=guarded_dp)
-
-        if report_cs <= 639:
+    # Helper Method for monthly_pmi_calculation
+    def credit_to_pmi_range(self, credit_score, pmi):
+        if credit_score <= 639:
             collector = pmi.range_620_639
-        elif report_cs in range(640, 660):
+        elif credit_score in range(640, 660):
             collector = pmi.range_640_659
-        elif report_cs in range(660, 680):
+        elif credit_score in range(660, 680):
             collector = pmi.range_660_679
-        elif report_cs in range(680, 700):
+        elif credit_score in range(680, 700):
             collector = pmi.range_680_699
-        elif report_cs in range(700, 720):
+        elif credit_score in range(700, 720):
             collector = pmi.range_700_719
-        elif report_cs in range(720, 740):
+        elif credit_score in range(720, 740):
             collector = pmi.range_720_739
-        elif report_cs in range(740, 760):
+        elif credit_score in range(740, 760):
             collector = pmi.range_740_759
-        elif report_cs in range(760, 851):
+        elif credit_score in range(760, 851):
             collector = pmi.range_760_850
+        return collector
 
+    # Helper Method for pmi && percentage_saved_based_on_principal && downpayment_goal_monthly_savings
+    def principal_imaginative_or_pragmatic(self):
         if self.goal_principal == 0:
             principal = self.principal_based_on_rent()
         else:
             principal = self.goal_principal
+        return principal
 
-        reduced_principal = (1 - (pmi.downpayment_percentage / 100)) * principal
-        annual_pmi = reduced_principal * (collector / 100)
-        monthly_pmi = (annual_pmi / 12)
-        return round(monthly_pmi)
+    # ReportController stylized report function call
+    def percentage_saved_based_on_principal(self):
+        principal = self.principal_imaginative_or_pragmatic()
+        percent_saved = (self.downpayment_savings / principal) * 100
+        return round(percent_saved, 2)
 
+    # ReportController stylized report function call
+    def downpayment_goal_monthly_savings(self, year):
+        if year == 0:
+            return 0
+
+        principal = self.principal_imaginative_or_pragmatic()
+        potential_downpayment = principal * (self.downpayment_percentage / 100)
+
+        if self.downpayment_savings >= potential_downpayment:
+            downpayment = self.downpayment_savings
+
+        downpayment = (principal - self.downpayment_savings) * (self.downpayment_percentage / 100)
+        monthly_goal = downpayment / (year * 12)
+        return round(monthly_goal)
+
+    # ReportController stylized report function call
     def true_monthly(self, city_state):
         tm = self.rent + self.home_insurance(city_state) + self.property_tax(city_state)
         if self.downpayment_percentage < 20:
             tm += self.pmi()
         return tm
 
+    # Helper Method for number_of_years && ReportController stylized report function call
     def monthly_principal(self):
         if self.rent != 0:
             return self.rent
@@ -129,23 +188,7 @@ class Report(db.Document):
         monthly = numerator / denominator
         return round(monthly, 2)
 
-    def percentage_saved_based_on_principal(self):
-        if self.goal_principal == 0.0:
-            principal = self.principal_based_on_rent()
-        else:
-            principal = self.goal_principal
-        percent_saved = (self.downpayment_savings / principal) * 100
-        return round(percent_saved, 2)
-
-    def downpayment_savings_goal_end_date(self, year):
-        now = datetime.datetime.now()
-        month = now.month
-        current_year = now.year
-        day = now.day
-        new_year = current_year + year
-        date = f'{month}/{day}/{new_year}'
-        return date
-
+    # Helper Method for number_of_years && principal_imaginative_or_pragmatic && ReportController stylized report function call
     def principal_based_on_rent(self):
         if self.goal_principal != 0:
             return 0
@@ -157,18 +200,21 @@ class Report(db.Document):
         numerator = (exponent - 1) * (monthly)
         denominator = (monthly_interest) * (exponent) * (percentage)
         imaginative_principal = numerator / denominator
+        if imaginative_principal < 30000:
+            imaginative_principal = 30000
         return round(imaginative_principal)
 
-    def downpayment_goal_monthly_savings(self, year):
-        if self.goal_principal == 0.0:
-            principal = self.principal_based_on_rent()
-        else:
-            principal = self.goal_principal
+    # ReportController stylized report function call
+    def downpayment_savings_goal_end_date(self, year):
+        now = datetime.datetime.now()
+        month = now.month
+        current_year = now.year
+        day = now.day
+        new_year = current_year + year
+        date = f'{month}/{day}/{new_year}'
+        return date
 
-        downpayment = (principal - self.downpayment_savings) * (self.downpayment_percentage / 100)
-        monthly_goal = downpayment / (year * 12)
-        return round(monthly_goal)
-
+    # ReportController stylized report function call
     def number_of_years(self, savings_style):
         monthly_pay = self.salary
         monthly_debt = self.monthly_debt
@@ -181,15 +227,24 @@ class Report(db.Document):
             principal = self.principal_based_on_rent()
 
         remaining_monthly = monthly_pay - monthly_debt - monthly_living_expense
+
+        if remaining_monthly < 500:
+            remaining_monthly = 500
+
         savings_cap = remaining_monthly * savings_style
 
-        downpayment = (principal - self.downpayment_savings) * (self.downpayment_percentage / 100)
-        potential_monthly_savings = downpayment / 12
-        year = 1
-        static_monthly = potential_monthly_savings
-        while potential_monthly_savings > savings_cap:
-            potential_monthly_savings = static_monthly / year
-            year += 1
+        potential_downpayment = principal * (self.downpayment_percentage / 100)
+
+        if self.downpayment_savings >= potential_downpayment:
+            dynamic_years = (0, 0, 0)
         else:
-            dynamic_years = (year, year + 2,  year + 4)
+            downpayment = (principal - self.downpayment_savings) * (self.downpayment_percentage / 100)
+            potential_monthly_savings = downpayment / 12
+            year = 1
+            static_monthly = potential_monthly_savings
+            while potential_monthly_savings > savings_cap:
+                potential_monthly_savings = static_monthly / year
+                year += 1
+            else:
+                dynamic_years = (year, year + 2,  year + 4)
         return dynamic_years
