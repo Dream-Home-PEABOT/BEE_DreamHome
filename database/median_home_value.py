45306136105cddbe1c59963925d8a1a5a87ee46e
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2021-01-30 08:54:04 -0600

    All seeds added and verified

--- database/median_home_value.py
+++ database/median_home_value.py
@@ -7,6 +7,7 @@ alabama = {
     "median_bottom_tier": 68371,
     "median_condo_value": 198697
 }
+
 alaska = {
     "year": 2020,
     "state": "Alaska",
@@ -16,6 +17,7 @@ alaska = {
     "median_bottom_tier": 191152,
     "median_condo_value": 206374
 }
+
 arizona = {
     "year": 2020,
     "state": "Arizona",
@@ -25,6 +27,7 @@ arizona = {
     "median_bottom_tier": 197259,
     "median_condo_value": 189677
 }
+
 arkansas = {
     "year": 2020,
     "state": "Arkansas",
@@ -34,6 +37,7 @@ arkansas = {
     "median_bottom_tier": 64097,
     "median_condo_value": 152569
 }
+
 california = {
     "year": 2020,
     "state": "California",
@@ -43,6 +47,7 @@ california = {
     "median_bottom_tier": 588050,
     "median_condo_value": 525594
 }
+
 colorado = {
     "year": 2020,
     "state": "Colorado",
@@ -52,6 +57,7 @@ colorado = {
     "median_bottom_tier": 279396,
     "median_condo_value": 315402
 }
+
 connecticut = {
     "year": 2020,
     "state": "Connecticut",
@@ -61,6 +67,7 @@ connecticut = {
     "median_bottom_tier": 170615,
     "median_condo_value": 164659
 }
+
 delaware = {
     "year": 2020,
     "state": "Delaware",
@@ -70,6 +77,7 @@ delaware = {
     "median_bottom_tier": 179496,
     "median_condo_value": 214201
 }
+
 florida = {
     "year": 2020,
     "state": "Florida",
@@ -79,6 +87,37 @@ florida = {
     "median_bottom_tier": 158180,
     "median_condo_value": 194264
 }
+
+georgia = {
+    "year": 2020,
+    "state": "Georgia",
+    "avg_home_value": 208833,
+    "median_top_tier": 365322,
+    "median_single_family_value": 208466,
+    "median_bottom_tier": 117107,
+    "median_condo_value": 216612
+}
+
+hawaii = {
+    "year": 2020,
+    "state": "Hawaii",
+    "avg_home_value": 646733,
+    "median_top_tier": 1003485,
+    "median_single_family_value": 727931,
+    "median_bottom_tier": 380055,
+    "median_condo_value": 459264
+}
+
+idaho = {
+    "year": 2020,
+    "state": "Idaho",
+    "avg_home_value": 303606,
+    "median_top_tier": 464079,
+    "median_single_family_value": 304036,
+    "median_bottom_tier": 205803,
+    "median_condo_value": 281977
+}
+
 illinois = {
     "year": 2020,
     "state": "Illinois",
@@ -87,4 +126,374 @@ illinois = {
     "median_single_family_value": 205201,
     "median_bottom_tier": 105910,
     "median_condo_value": 201587
+}
+
+indiana = {
+    "year": 2020,
+    "state": "Indiana",
+    "avg_home_value": 161106,
+    "median_top_tier": 271339,
+    "median_single_family_value": 161291,
+    "median_bottom_tier": 88260,
+    "median_condo_value": 156609
+}
+
+iowa = {
+    "year": 2020,
+    "state": "Iowa",
+    "avg_home_value": 155781,
+    "median_top_tier": 265520,
+    "median_single_family_value": 157261,
+    "median_bottom_tier": 83788,
+    "median_condo_value": 137156
+}
+
+kansas = {
+    "year": 2020,
+    "state": "Kansas",
+    "avg_home_value": 154531,
+    "median_top_tier": 287863,
+    "median_single_family_value": 154777,
+    "median_bottom_tier": 71887,
+    "median_condo_value": 135736
+}
+
+kentucky = {
+    "year": 2020,
+    "state": "Kentucky",
+    "avg_home_value": 150918,
+    "median_top_tier": 261262,
+    "median_single_family_value": 150877,
+    "median_bottom_tier": 77463,
+    "median_condo_value": 150920
+}
+
+louisiana = {
+    "year": 2020,
+    "state": "Louisiana",
+    "avg_home_value": 171485,
+    "median_top_tier": 296599,
+    "median_single_family_value": 171948,
+    "median_bottom_tier": 94364,
+    "median_condo_value": 153199
+}
+
+maine = {
+    "year": 2020,
+    "state": "Maine",
+    "avg_home_value": 253914,
+    "median_top_tier": 422248,
+    "median_single_family_value": 252014,
+    "median_bottom_tier": 150408,
+    "median_condo_value": 278898
+}
+
+maryland = {
+    "year": 2020,
+    "state": "Maryland",
+    "avg_home_value": 317033,
+    "median_top_tier": 513094,
+    "median_single_family_value": 327604,
+    "median_bottom_tier": 192725,
+    "median_condo_value": 224299
+}
+
+massachusetts = {
+    "year": 2020,
+    "state": "Massachusetts",
+    "avg_home_value": 439541,
+    "median_top_tier": 732598,
+    "median_single_family_value": 447367,
+    "median_bottom_tier": 283343,
+    "median_condo_value": 393196
+}
+
+michigan = {
+    "year": 2020,
+    "state": "Michigan",
+    "avg_home_value": 180404,
+    "median_top_tier": 313720,
+    "median_single_family_value": 177403,
+    "median_bottom_tier": 90535,
+    "median_condo_value": 211352
+}
+
+minnesota = {
+    "year": 2020,
+    "state": "Minnesota",
+    "avg_home_value": 266887,
+    "median_top_tier": 403999,
+    "median_single_family_value": 270308,
+    "median_bottom_tier": 169614,
+    "median_condo_value": 191041
+}
+
+mississippi = {
+    "year": 2020,
+    "state": "Mississippi",
+    "avg_home_value": 126511,
+    "median_top_tier": 223621,
+    "median_single_family_value": 126294,
+    "median_bottom_tier": 68093,
+    "median_condo_value": 145414
+}
+
+missouri = {
+    "year": 2020,
+    "state": "Missouri",
+    "avg_home_value": 167700,
+    "median_top_tier": 288175,
+    "median_single_family_value": 168392,
+    "median_bottom_tier": 90857,
+    "median_condo_value": 155283
+}
+
+montana = {
+    "year": 2020,
+    "state": "Montana",
+    "avg_home_value": 293685,
+    "median_top_tier": 464744,
+    "median_single_family_value": 293244,
+    "median_bottom_tier": 185975,
+    "median_condo_value": 305874
+}
+
+nebraska = {
+    "year": 2020,
+    "state": "Nebraska",
+    "avg_home_value": 180397,
+    "median_top_tier": 291471,
+    "median_single_family_value": 180496,
+    "median_bottom_tier": 104964,
+    "median_condo_value": 174687
+}
+
+nevada = {
+    "year": 2020,
+    "state": "Nevada",
+    "avg_home_value": 311018,
+    "median_top_tier": 458950,
+    "median_single_family_value": 322809,
+    "median_bottom_tier": 223577,
+    "median_condo_value": 168460
+}
+
+new_hampshire = {
+    "year": 2020,
+    "state": "New Hampshire",
+    "avg_home_value": 303875,
+    "median_top_tier": 456232,
+    "median_single_family_value": 315079,
+    "median_bottom_tier": 199074,
+    "median_condo_value": 231709
+}
+
+new_jersey = {
+    "year": 2020,
+    "state": "New Jersey",
+    "avg_home_value": 346368,
+    "median_top_tier": 584459,
+    "median_single_family_value": 355423,
+    "median_bottom_tier": 205231,
+    "median_condo_value": 275174
+}
+
+new_mexico = {
+    "year": 2020,
+    "state": "New Mexico",
+    "avg_home_value": 214213,
+    "median_top_tier": 358768,
+    "median_single_family_value": 215378,
+    "median_bottom_tier": 142717,
+    "median_condo_value": 180527
+}
+
+new_york = {
+    "year": 2020,
+    "state": "New York",
+    "avg_home_value": 331459,
+    "median_top_tier": 728940,
+    "median_single_family_value": 275426,
+    "median_bottom_tier": 145097,
+    "median_condo_value": 593067
+}
+
+north_carolina = {
+    "year": 2020,
+    "state": "North Carolina",
+    "avg_home_value": 210766,
+    "median_top_tier": 366161,
+    "median_single_family_value": 212247,
+    "median_bottom_tier": 115378,
+    "median_condo_value": 172189
+}
+
+north_dakota = {
+    "year": 2020,
+    "state": "North Dakota",
+    "avg_home_value": 236405,
+    "median_top_tier": 347710,
+    "median_single_family_value": 241803,
+    "median_bottom_tier": 155757,
+    "median_condo_value": 154687
+}
+
+ohio = {
+    "year": 2020,
+    "state": "Ohio",
+    "avg_home_value": 156343,
+    "median_top_tier": 275136,
+    "median_single_family_value": 157386,
+    "median_bottom_tier": 83321,
+    "median_condo_value": 144814
+}
+
+oklahoma = {
+    "year": 2020,
+    "state": "Oklahoma",
+    "avg_home_value": 134289,
+    "median_top_tier": 235037,
+    "median_single_family_value": 134995,
+    "median_bottom_tier": 64895,
+    "median_condo_value": 83881
+}
+
+oregon = {
+    "year": 2020,
+    "state": "Oregon",
+    "avg_home_value": 374604,
+    "median_top_tier": 550478,
+    "median_single_family_value": 377275,
+    "median_bottom_tier": 263013,
+    "median_condo_value": 302959
+}
+
+pennsylvania = {
+    "year": 2020,
+    "state": "Pennsylvania",
+    "avg_home_value": 200367,
+    "median_top_tier": 351542,
+    "median_single_family_value": 201300,
+    "median_bottom_tier": 108661,
+    "median_condo_value": 185901
+}
+
+rhode_island = {
+    "year": 2020,
+    "state": "Rhode Island",
+    "avg_home_value": 312255,
+    "median_top_tier": 495252,
+    "median_single_family_value": 317918,
+    "median_bottom_tier": 236324,
+    "median_condo_value": 246972
+}
+
+south_carolina = {
+    "year": 2020,
+    "state": "South Carolina",
+    "avg_home_value": 193491,
+    "median_top_tier": 346615,
+    "median_single_family_value": 195761,
+    "median_bottom_tier": 106900,
+    "median_condo_value": 165922
+}
+
+south_dakota = {
+    "year": 2020,
+    "state": "South Dakota",
+    "avg_home_value": 217215,
+    "median_top_tier": 334102,
+    "median_single_family_value": 217789,
+    "median_bottom_tier": 143611,
+    "median_condo_value": 169650
+}
+
+tennessee = {
+    "year": 2020,
+    "state": "Tennessee",
+    "avg_home_value": 192275,
+    "median_top_tier": 338100,
+    "median_single_family_value": 191164,
+    "median_bottom_tier": 99130,
+    "median_condo_value": 215736
+}
+
+texas = {
+    "year": 2020,
+    "state": "Texas",
+    "avg_home_value": 213036,
+    "median_top_tier": 367998,
+    "median_single_family_value": 213666,
+    "median_bottom_tier": 126071,
+    "median_condo_value": 185154
+}
+
+utah = {
+    "year": 2020,
+    "state": "Utah",
+    "avg_home_value": 360044,
+    "median_top_tier": 540784,
+    "median_single_family_value": 365960,
+    "median_bottom_tier": 262743,
+    "median_condo_value": 300670
+}
+
+vermont = {
+    "year": 2020,
+    "state": "Vermont",
+    "avg_home_value": 264777,
+    "median_top_tier": 404897,
+    "median_single_family_value": 269754,
+    "median_bottom_tier": 171962,
+    "median_condo_value": 235008
+}
+
+virginia = {
+    "year": 2020,
+    "state": "Virginia",
+    "avg_home_value": 293818,
+    "median_top_tier": 547212,
+    "median_single_family_value": 295926,
+    "median_bottom_tier": 165069,
+    "median_condo_value": 278124
+}
+
+washington = {
+    "year": 2020,
+    "state": "Washington",
+    "avg_home_value": 431487,
+    "median_top_tier": 723762,
+    "median_single_family_value": 289328,
+    "median_bottom_tier": 437442,
+    "median_condo_value": 368134
+}
+
+west_virginia = {
+    "year": 2020,
+    "state": "West Virginia",
+    "avg_home_value": 107064,
+    "median_top_tier": 199651,
+    "median_single_family_value": 106840,
+    "median_bottom_tier": 52166,
+    "median_condo_value": 126199
+}
+
+wisconsin = {
+    "year": 2020,
+    "state": "Wisconsin",
+    "avg_home_value": 206169,
+    "median_top_tier": 330532,
+    "median_single_family_value": 207906,
+    "median_bottom_tier": 129163,
+    "median_condo_value": 188368
+}
+
+wyoming = {
+    "year": 2020,
+    "state": "Wyoming",
+    "avg_home_value": 254753,
+    "median_top_tier": 412188,
+    "median_single_family_value": 254065,
+    "median_bottom_tier": 167987,
+    "median_condo_value": 325907
 }
