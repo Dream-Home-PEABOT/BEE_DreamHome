a16394e365b928c6c9e158f33d91fdde0a00a0de
Priya Power <49959312+priyapower@users.noreply.github.com>
2021-01-29 18:07:53 -0700

    Add seed data for median home value
    - Doesn't include all 50 states yet
    
    Co-Authored-By: Arique <Arique1104@users.noreply.github.com>

---
+++ database/median_home_value.py
@@ -0,0 +1,90 @@
+alabama = {
+    "year": 2020,
+    "state": "Alabama",
+    "avg_home_value": 147539,
+    "median_top_tier": 271889,
+    "median_single_family_value": 146534,
+    "median_bottom_tier": 68371,
+    "median_condo_value": 198697
+}
+alaska = {
+    "year": 2020,
+    "state": "Alaska",
+    "avg_home_value": 297111,
+    "median_top_tier": 428907,
+    "median_single_family_value": 311194,
+    "median_bottom_tier": 191152,
+    "median_condo_value": 206374
+}
+arizona = {
+    "year": 2020,
+    "state": "Arizona",
+    "avg_home_value": 283623,
+    "median_top_tier": 449443,
+    "median_single_family_value": 290075,
+    "median_bottom_tier": 197259,
+    "median_condo_value": 189677
+}
+arkansas = {
+    "year": 2020,
+    "state": "Arkansas",
+    "avg_home_value": 131837,
+    "median_top_tier": 232877,
+    "median_single_family_value": 131577,
+    "median_bottom_tier": 64097,
+    "median_condo_value": 152569
+}
+california = {
+    "year": 2020,
+    "state": "California",
+    "avg_home_value": 579332,
+    "median_top_tier": 1065479,
+    "median_single_family_value": 346944,
+    "median_bottom_tier": 588050,
+    "median_condo_value": 525594
+}
+colorado = {
+    "year": 2020,
+    "state": "Colorado",
+    "avg_home_value": 412819,
+    "median_top_tier": 642646,
+    "median_single_family_value": 422369,
+    "median_bottom_tier": 279396,
+    "median_condo_value": 315402
+}
+connecticut = {
+    "year": 2020,
+    "state": "Connecticut",
+    "avg_home_value": 262600,
+    "median_top_tier": 456808,
+    "median_single_family_value": 278008,
+    "median_bottom_tier": 170615,
+    "median_condo_value": 164659
+}
+delaware = {
+    "year": 2020,
+    "state": "Delaware",
+    "avg_home_value": 262667,
+    "median_top_tier": 394260,
+    "median_single_family_value": 264627,
+    "median_bottom_tier": 179496,
+    "median_condo_value": 214201
+}
+florida = {
+    "year": 2020,
+    "state": "Florida",
+    "avg_home_value": 254607,
+    "median_top_tier": 432633,
+    "median_single_family_value": 266350,
+    "median_bottom_tier": 158180,
+    "median_condo_value": 194264
+}
+illinois = {
+    "year": 2020,
+    "state": "Illinois",
+    "avg_home_value": 204872,
+    "median_top_tier": 362334,
+    "median_single_family_value": 205201,
+    "median_bottom_tier": 105910,
+    "median_condo_value": 201587
+}
